52edaeeb693e50e022f99df931a162a5cc80d192
Milkeez <malicklam1197@gmail.com>
2022-03-30 11:27:10 +0000

    validation suppression des colonnes

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ const btnlancement=document.querySelector('.btnlancement');
 const nav=document.querySelector('nav');
 const note=document.querySelector('.note');
 const colonne=document.querySelector('.Colonne');
+const textarea=document.querySelector('textarea');
+const date=document.getElementById('date')
+const hdebut=document.getElementById('hdebut');
+const hfin=document.getElementById('hfin');
+const valider=document.querySelector('.form').lastElementChild;
 
+
+/* valider.addEventListener('click',function(){
+   
+})
+ */
 const modalContainer=document.getElementById('modal-container');
 const suppModal=document.querySelector('.suppModal');
 const modal=document.querySelector('.modal');
+const tab=['#DBA576','#e864ad8a','#5C322C','#36798F','#488C66']
 
 
 const main=document.getElementById('main');
 var i=0;
 
 btnlancement.addEventListener('click',function(){
+    alert('ok')
     nav.classList.toggle('show-nav')
 })
 
 colonne.addEventListener('click',function(){
     if (i<5) {
         i++;
-        createColonne(i);
+        createColonne();
+        
     }else{
         alert('vous avez atteint le nombre maximum de colonnes')
     }
@@ -36,11 +49,10 @@ note.addEventListener('click',function(){
         modalContainer.classList.remove('show-modal')
        
     })
-
-    
+   
 })
 
-function createColonne(i) {
+function createColonne() {
 
     const spanTitre=document.createElement('span');
     spanTitre.innerText=`Colonne ${i}`
@@ -55,7 +67,8 @@ function createColonne(i) {
     divContainer.setAttribute('class','container');
     divColonne.setAttribute('class','colonne');
     divColonne.classList.add('pullUp');
-    divColonne.classList.add(`c${i}`);
+    suppColonne.classList.add(`c${i}`)
+    divColonne.style.backgroundColor=tab[i-1];
 
 
 
@@ -64,5 +77,34 @@ function createColonne(i) {
     divColonne.appendChild(divTitre);
     divColonne.appendChild(divContainer);
     main.appendChild(divColonne);
+    /*  const c1=document.querySelector('.c1'); */
+    
+     suppColonne.addEventListener('click',function(e){
+        var tab=e.target.classList;
+        if (tab[1]!='c1') {
+            e.target.parentElement.parentElement.remove();
+            i--;
+            reflesh();
+        }else{
+            if (e.target.parentElement.parentElement.nextSibling==null) {
+                e.target.parentElement.parentElement.remove();
+                i--;
+                reflesh();
+            }else{
+                 alert('Veillez supprimer les autres pour pouvoir supprimer cette colonne')
+            }
+        }
+    })
+}
 
+
+function reflesh() {
+    const titre=document.querySelectorAll('.titre');
+    titre.forEach((elm,k)=>{
+        elm.firstChild.innerText=`Colonne ${k+1}`;
+        elm.parentElement.style.backgroundColor=tab[k]
+        
+    })
+   
+  
 }
